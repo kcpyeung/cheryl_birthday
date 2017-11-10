0d1d777c2d8d7a00251f8a800bf1fbc418e3a101
Kevin Yeung <github@yeung.online>
2017-11-10 21:53:43 +0800

    Add tell

--- test/cheryl_birthday/core_test.clj
+++ test/cheryl_birthday/core_test.clj
@@ -12,4 +12,11 @@
   (testing "having exactly one option is knowing"
     (is (know? ["May 6"]))))
 
+(deftest test-tell
+  (let [dates ["May 15", "May 16", "May 19", "June 17", "June 18", "July 14", "July 16", "August 14", "August 15", "August 17"]]
+    (testing "telling the month narrows down possible answers to that month"
+      (is (= ["May 15" "May 16" "May 19"] (tell "May" dates))))
+    (testing "telling the day narrows down possible answers to that day"
+      (is (= ["May 15" "August 15"] (tell "15" dates))))))
+
 (run-tests)
