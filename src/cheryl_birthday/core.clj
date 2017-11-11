8c7839f1c658fabb4973272bf0d3116d33f00ce1
Kevin Yeung <github@yeung.online>
2017-11-11 13:25:26 +0800

    Albert: I don't know when Cheryl's birthday is, but I know that Bernard does not know too.

--- src/cheryl_birthday/core.clj
+++ src/cheryl_birthday/core.clj
@@ -12,3 +12,10 @@
 
 (defn day [date]
   (second (s/split date #" ")))
+
+(defn albert-doesn't-know-and-he-knows-bernard-can't-tell-either [date all-dates]
+  (let [what-albert-narrows-down-to (tell (month date) all-dates)
+        all-days-in-albert's-list (distinct (map day what-albert-narrows-down-to))
+        what-albert-thinks-bernard-can-narrow-down-to (map #(tell % all-dates) all-days-in-albert's-list)
+        bernard-can't-identify-a-unique-date (not-any? know? what-albert-thinks-bernard-can-narrow-down-to)]
+    (and (not (know? what-albert-narrows-down-to)) bernard-can't-identify-a-unique-date)))
--- test/cheryl_birthday/core_test.clj
+++ test/cheryl_birthday/core_test.clj
@@ -3,6 +3,8 @@
             [cheryl-birthday.core :refer :all]
             [clojure.string :as s]))
 
+(def dates ["May 15", "May 16", "May 19", "June 17", "June 18", "July 14", "July 16", "August 14", "August 15", "August 17"])
+
 (deftest test-know?
   (testing "having muliple options is not knowing"
     (is (not (know? ["May 3" "May 6"]))))
@@ -14,14 +16,21 @@
     (is (know? ["May 6"]))))
 
 (deftest test-tell
-  (let [dates ["May 15", "May 16", "May 19", "June 17", "June 18", "July 14", "July 16", "August 14", "August 15", "August 17"]]
-    (testing "telling the month narrows down possible answers to that month"
-      (is (= ["May 15" "May 16" "May 19"] (tell "May" dates))))
-    (testing "telling using month"
-      (is (= ["May 15" "May 16" "May 19"] (tell (month "May 15") dates))))
-    (testing "telling the day narrows down possible answers to that day"
-      (is (= ["May 15" "August 15"] (tell "15" dates))))
-    (testing "telling using day"
-      (is (= ["May 15" "August 15"] (tell (day "May 15") dates))))))
+  (testing "telling the month narrows down possible answers to that month"
+    (is (= ["May 15" "May 16" "May 19"] (tell "May" dates))))
+  (testing "telling using month"
+    (is (= ["May 15" "May 16" "May 19"] (tell (month "May 15") dates))))
+  (testing "telling the day narrows down possible answers to that day"
+    (is (= ["May 15" "August 15"] (tell "15" dates))))
+  (testing "telling using day"
+    (is (= ["May 15" "August 15"] (tell (day "May 15") dates)))))
+
+(deftest test-albert-doesn't-know-and-he-knows-bernard-can't-tell-either
+  (testing "Albert doesn't know if there are multiple dates in that month"
+    (is (albert-doesn't-know-and-he-knows-bernard-can't-tell-either "August 15" dates)))
+  (testing "Bernard can't tell if there are multiple dates on that day"
+    (is (albert-doesn't-know-and-he-knows-bernard-can't-tell-either "August 15" dates)))
+  (testing "it cannot be June because otherwise Bernard might have a unique answer in June 18"
+    (is (not (albert-doesn't-know-and-he-knows-bernard-can't-tell-either "June 17" dates)))))
 
 (run-tests)
